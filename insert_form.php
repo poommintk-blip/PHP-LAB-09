ffea405a5d8cb7a93e9d14b6608cf8fd449d5b36
Poommin.tk <poommin.tk@gmail.com>
2025-12-19 23:05:15 +0700

    Add files via upload

---
+++ insert_form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+</head>
+<body>
+<h3> แบบฟอร์มข้อมูลหนังสือ </h3>
+<form name="form1" method="post" action="insert.php">
+Book ID: <input type="text" name="book_id"><br />
+Book Name: <input type="text" name="book_name"><br />
+Author: <input type="text" name="author"><br />
+Publisher: <input type="text" name="publisher"><br />
+Price: <input type="text" name="price"><br />
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html>
