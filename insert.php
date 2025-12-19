e97e2e056d5882a54b01a4694a183f5a330e27d7
Poommin.tk <poommin.tk@gmail.com>
2025-12-19 23:08:45 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+$con = mysqli_connect("localhost", "root", "");
+if(!$con) {
+die("Could not connect: "
+. mysqli_connect_error());
+}
+mysqli_select_db($con, "library");
+// สั่งคำสั่ง SQL เพิ่มข้อมูล
+$sql =
+"INSERT INTO book (book_id, book_name) VALUES('" . $_POST['book_id'] . "', '" . $_POST['book_name'] . "')";
+mysqli_query($con, $sql);
+echo "1 record added!<br /><br />";
+mysqli_close($con);
+?>
+</body>
+</html>
