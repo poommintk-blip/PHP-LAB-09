3cbe0672550005ef478c5522f473b1ae67acc04a
Poommin.tk <poommin.tk@gmail.com>
2025-12-19 23:09:47 +0700

    Add files via upload

---
+++ update_form.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+</head>
+<body>
+Update Price<br />
+<form name="form1" method="post" action="update.php">
+Book ID: <input type="text" name="book_id"><br />
+New Price: <input type="text" name="price"><br />
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html>
