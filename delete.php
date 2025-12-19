54a6c09ccd4eec2a9f1339b1e7e16c25ca5a6e75
Poommin.tk <poommin.tk@gmail.com>
2025-12-19 23:16:28 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+</head>
+<body>
+<?php
+$ $con = mysqli_connect("localhost", "root", "");
+if(!$con) {
+die("Could not connect: "
+. mysql_error());
+}
+mysqli_select_db($con, "library");
+// 3) คำสั่ง SQL ลบข้อมูล
+$sql="DELETE FROM book WHERE book_id = '" . $_POST["book_id"] . "'";
+
+if(!mysqli_query($con, $sql)) {die("Error: " . mysqli_error($con));
+} else {
+echo "1 record deleted.";
+}
+mysqli_close($con);
+?>
+</body>
+</html>
---
+++ delete_form.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+</head>
+<body>
+Delete Book<br />
+<form name="form1" method="post" action="delete.php">
+Book ID: <input type="text" name="book_id"><br />
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html>
