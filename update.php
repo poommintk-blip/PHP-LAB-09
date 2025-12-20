0dfa7af2b79c12a525987b415e5f290fac5725d5
Poommin.tk <poommin.tk@gmail.com>
2025-12-20 14:14:57 +0700

    Add files via upload

--- update.php
+++ update.php
@@ -1,19 +1,31 @@
 <html>
 <body>
 <?php
-if(!$con) {
-$con = mysqli_connect ("localhost", "root", "default", "library");
-die("Could not connect: " . mysqli_connect_error());
+$con = mysqli_connect("localhost", "root", "", "library");
+if (mysqli_connect_errno()) {
+    die("Could not connect: " . mysqli_connect_error());
 }
+mysqli_set_charset($con, "utf8");
 mysqli_select_db($con, "library");
-// คำสั่ง SQL แก้ไขข้อมูล
-$sql="UPDATE book SET price = "
-. $_POST["price"] . " WHERE book_id = '" . $_POST["book_id"] . "'";
-if(!mysqli_query($con, $sql)) {
-die("Error: " . mysqli_connect_error($con));
+if(isset($_POST["price"]) && isset($_POST["book_id"])) {
+    
+    $price = $_POST["price"];
+    $book_id = $_POST["book_id"];
+
+    $sql = "UPDATE book SET price = " . $price . " WHERE book_id = '" . $book_id . "'";
+
+    // 4. รันคำสั่ง SQL
+    if (!mysqli_query($con, $sql)) {
+        // แก้จาก mysqli_connect_error เป็น mysqli_error($con)
+        die("Error: " . mysqli_error($con)); 
+    } else {
+        echo "1 record updated.";
+    }
+
 } else {
-echo "1 record updated.";
+    echo "ไม่พบข้อมูลที่ส่งมา (Price หรือ Book ID หายไป)";
 }
+
 mysqli_close($con);
 ?>
 </body>
